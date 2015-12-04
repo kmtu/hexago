30ca9f47961465edd8a613c18397d127d38b13bf
KM Tu <tutu522@gmail.com>
2015-12-04 15:37:48 +0800

    Adds a Move class representing both Place and Shift movement

--- hexago.py
+++ hexago.py
@@ -1,6 +1,41 @@
 import os
 import re
 import sys
+from enum import Enum
+
+class Move():
+    class Type(Enum):
+        place = 'place'
+        shift = 'shift'
+
+    def __init__(self, type, player):
+        self.type = type
+        self.player = player
+
+
+class Place(Move):
+    def __init__(self, player, position):
+        super().__init__(Move.Type.place, player)
+        self.position = position
+
+
+class Shift(Move):
+    class Direction(Enum):
+        up = '^'
+        down = 'v'
+        left = '<'
+        right = '>'
+
+    class Side(Enum):
+        positive = '+'
+        negative = '-'
+
+    def __init__(self, player, direction, line, side, displace):
+        super().__init__(Move.Type.shift, player)
+        self.direction = direction
+        self.line = line
+        self.side = side
+        self.displace = displace
 
 
 class Player():
@@ -24,8 +59,25 @@ class Board():
             else:
                 print('├─' + '─┼─'.join(['─' for j in row]) + '─┤')
 
-    def put(self, player, position):
-        self.table[position[0]][position[1]] = player.symbol
+
+    def move(self, move):
+        player = move.player
+        if move.type is Move.Type.place:
+            self.place(move)
+        elif move.type is Move.Type.shift:
+            self.shift(move)
+
+    def place(self, move):
+        player = move.player
+        pos = move.position
+        self.table[pos[0]][pos[1]] = player.symbol
+
+    def shift(self, move):
+        player = move.player
+        direction = move.direction
+        line = move.line
+        side = move.side
+        displace = move.displace
 
 
 class Game():
@@ -73,7 +125,7 @@ class Game():
                     else:
                         print("Invalid input!")
 
-            self.board.put(current_player, pos)
+            self.board.move(Place(current_player, pos))
 
             # change to next player
             self.current_player_id += 1
